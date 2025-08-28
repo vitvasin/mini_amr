ff906caf989a2ded67f9358382260b20e98b0c05
Vitvasin <vasin22315@hotmail.com>
2025-08-28 16:18:47 +0700

    Update navigation script and adjust navigation parameters for improved performance

--- Shortcut/navigation_amcl.sh
+++ Shortcut/navigation_amcl.sh
@@ -1,3 +1,4 @@
 #ros2 launch navigation navigation.launch.py map:="$ROS_WS/maps/smr_room" rviz:=true rviz_config_file:="$ROS_WS/install/navigation/share/navigation/rviz/rviz_nav.rviz"
 #ros2 launch navigation navigation.launch.py use_slam_tb:=false use_mapping:=false rviz:=true rviz_config_file:="$ROS_WS/install/navigation/share/navigation/rviz/rviz_nav.rviz" map:="/home/emr/workspaces/mini_amr/Shortcut/410_amcl.yaml"
-ros2 launch navigation amcl_localization_launch.py map:=/home/emr/workspaces/mini_amr/Shortcut/410_amcl.yaml
+#ros2 launch navigation amcl_localization_launch.py map:=/home/emr/workspaces/mini_amr/Shortcut/410_amcl.yaml
+./navigation_new.sh amcl

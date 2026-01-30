ab7f068643020b2dc077c3809992082f795be161
vitvasin <vasin22315@hotmail.com>
2026-01-30 14:48:13 +0700

    feat: Update navigation parameters, disable RViz launch, adjust hardware interface update rate, and modify database.

--- Shortcut/navigation.sh
+++ Shortcut/navigation.sh
@@ -1,5 +1,5 @@
 #ros2 launch navigation navigation.launch.py map:="/home/smr/Projects/maps/map" rviz:=true rviz_config_file:="$ROS_WS/install/navigation/share/navigation/rviz/rviz_nav.rviz"
-ros2 launch navigation navigation.launch.py map:="/home/smr/workspaces/mini_amr/amrROS2_ws/maps/NECTEC_4th_Floor_SLAM4" rviz:=true rviz_config_file:="$ROS_WS/install/navigation/share/navigation/rviz/rviz_nav.rviz"
+ros2 launch navigation navigation.launch.py map:="/home/smr/workspaces/mini_amr/amrROS2_ws/maps/NECTEC_4th_Floor_SLAM4" rviz:=false rviz_config_file:="$ROS_WS/install/navigation/share/navigation/rviz/rviz_nav.rviz"
 #ros2 launch navigation navigation.launch.py map:="/home/smr/workspaces/mini_amr/amrROS2_ws/maps/map410" rviz:=true rviz_config_file:="$ROS_WS/install/navigation/share/navigation/rviz/rviz_nav.rviz"
 
 

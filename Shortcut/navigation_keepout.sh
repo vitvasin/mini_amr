e0ad8aa5fcd29e52fc78cde4ce95fb246f0dbd4e
vitvasin <vasin22315@hotmail.com>
2026-04-28 15:42:53 +0700

    feat: migrate to MPPI controller and update navigation configuration parameters

--- Shortcut/navigation_keepout.sh
+++ Shortcut/navigation_keepout.sh
@@ -1,4 +1,4 @@
-ros2 launch navigation navigation.launch_keepout.py \
+ros2 launch navigation navigation_keepout.launch.py \
 map:="/home/smr/workspaces/mini_amr/amrROS2_ws/maps/NECTEC_4th_Floor_SLAM4" \
 rviz:=true \
 rviz_config_file:="$ROS_WS/install/navigation/share/navigation/rviz/rviz_nav.rviz" \
--- amrROS2_ws/src/navigation/launch/navigation_launch_keepout.py
+++ amrROS2_ws/src/navigation/launch/navigation_launch_keepout.py
@@ -29,6 +29,7 @@ from nav2_common.launch import RewrittenYaml
 def generate_launch_description():
     # Get the launch directory
     bringup_dir = get_package_share_directory('nav2_bringup')
+    navigation_dir = get_package_share_directory('navigation')
 
     namespace = LaunchConfiguration('namespace')
     use_sim_time = LaunchConfiguration('use_sim_time')
@@ -397,7 +398,8 @@ def generate_launch_description():
 
     declare_params_file_cmd = DeclareLaunchArgument(
         'params_file',
-        default_value=os.path.join(bringup_dir, 'params', 'nav2_params_keepout.yaml'),
+        # default_value=os.path.join(bringup_dir, 'params', 'nav2_params_keepout.yaml'),
+        default_value=os.path.join(navigation_dir, 'config', 'nav2_params_keepout.yaml'),
         description='Full path to the ROS2 parameters file to use for all launched nodes',
     )
 
